c8b1b269601da457ecf53fe1d75589331fcfc73c
Gian Alix <gian.alix@gmail.com>
2022-04-11 15:56:27 -0400

    Update dp_loss.py

--- model/dp_loss.py
+++ model/dp_loss.py
@@ -7,104 +7,111 @@ import numpy as np
 import random as rnd
 
 class DepthPredictionLoss(nn.Module):
-  
-  '''
-    pr_d: The predicted depth
-    gt_d: The ground truth depth
-  '''
-  def __init__(self, pr_d, gt_d):
-    super(DepthPredictionLoss, self).__init__()
-    self.d_bar = None
-    
-    # Image-level Normalized Regression Loss
-    self.ilnr = self.image_level_normalized_regression_loss(pr_d, gt_d)
-    
-    # Paiwise Normal Loss
-    sampled_points =  self.sample_pair_points(pr_d, 100000) # 100K sampled points
-    sample_gt_d_A = self.get_gt_d_of_sample(sampled_points, pr_d, gt_d)
-    surface_normal_A = self.surface_normal(sampled_points, sample_gt_d_A)
-    sampled_points =  self.sample_pair_points(pr_d, 100000) # 100K sampled points
-    sample_gt_d_B = self.get_gt_d_of_sample(sampled_points, pr_d, gt_d)
-    surface_normal_B = self.surface_normal(sampled_points, sample_gt_d_B)
-    self.pwn = self.pairwise_normal_loss(surface_normal_A, surface_normal_B, sample_gt_d_A, sample_gt_d_B)
-    
-    # Multi-scale Gradient Loss
-    self.msg = self.multi_scale_gradient_loss(gt_d, self.d_bar) # not sure about this
-    
-    self.overall_loss = self.overall_loss()
 
-  '''
-    Get the Image-level Normalized Regression Loss (ILNR) given the predicted and ground truth depths
-    1. Trim out values that are within the 10% furthest away
-    2. Apply normalization based on the mean and std of the remaining
-    This is done to prevent outliers.
-    3. Apply the proposed ILNR formula.
-  '''
-  def image_level_normalized_regression_loss(self, pr_d, gt_d):
-    gt_d_trim = torch.stack([x for x in gt_d if 0.1 < gt_d < 0.9])
-    d_bar = (gt_d - torch.mean(gt_d_trim))/torch.std(gt_d)
-    self.d_bar = d_bar
-    return torch.mean(torch.abs(pr_d - d_bar) + torch.abs(torch.tanh(pr_d/100) - torch.tanh(d_bar/100)))
+    '''
+        pr_d: The predicted depth
+        gt_d: The ground truth depth
+    '''
+    def __init__(self, pr_d, gt_d):
+        super(DepthPredictionLoss, self).__init__()
+        
+        self.pr_d = torch.Tensor(pr_d)
+        self.gt_d = torch.Tensor(gt_d)
+        
+        gt_d_trim = torch.clamp(self.gt_d, 0.1, 0.9)
+        mean_trim = torch.mean(gt_d_trim)
+        sd_trim = torch.std(gt_d_trim)
+        self.d_bar = (self.gt_d - mean_trim)/sd_trim
+        
+        # Image-level Normalized Regression Loss
+        self.ilnr = self.image_level_normalized_regression_loss()
 
-  '''
-    Sample [num] points from [pr_d]
-    where [num] is the number of points that we wish to sample.
-  '''
-  def sample_pair_points(self, pr_d, num):
-    idx = torch.multinomial(num_samples=num, replacement=False)
-    return pr_d[idx]
+        # Paiwise Normal Loss
+        sampled_points =  self.sample_pair_points(100000) # 100K sampled points
+        sample_gt_d_A = self.get_gt_d_of_sample(sampled_points)
+        surface_normal_A = self.surface_normal(sampled_points, sample_gt_d_A)
+        sampled_points =  self.sample_pair_points(100000) # 100K sampled points
+        sample_gt_d_B = self.get_gt_d_of_sample(sampled_points)
+        surface_normal_B = self.surface_normal(sampled_points, sample_gt_d_B)
+        self.pwn = self.pairwise_normal_loss(surface_normal_A, surface_normal_B, sample_gt_d_A, sample_gt_d_B)
 
-  '''
-    Get the ground truth of a sample [smp]
-    [smp] is a list
-  '''
-  def get_gt_d_of_sample(self, smp, pr_d, gt_d):
-    pos = [(pr_d == x).nonzero().item() for x in smp]   # get the pos list of the samples from  the pr_d
-    return torch.stack([gt_d[x] for x in pos])
+        # Multi-scale Gradient Loss
+        self.msg = self.multi_scale_gradient_loss() 
+        
+        self.overall_loss = self.overall_loss_func()
 
-  '''
-    Follows Xian et al's Structure-guided rank loss, which can imporve edge sharpness
-    the sampling method is followed but enforced on surface normal space
-    this improves global and local geometric relations
-  '''
-  def surface_normal(self, sample_pr_d, sample_gt_d):
-    # to-do
-    # requires 3D point cloud
-    # least squares fit
-    gr_d = sample_gt_d.squeeze()
-    pe_d = sample_pr_d.squeeze()
-    filtered = (gr_d > 1e-8) & (pe_d > 1e-8)
+    '''
+        Get the Image-level Normalized Regression Loss (ILNR) given the predicted and ground truth depths
+        1. Trim out values that are within the 10% furthest away
+        2. Apply normalization based on the mean and std of the remaining
+        This is done to prevent outliers.
+        3. Apply the proposed ILNR formula.
+    '''
+    def image_level_normalized_regression_loss(self):
+        return torch.mean(torch.abs(self.pr_d - self.d_bar) + torch.abs(torch.tanh(self.pr_d/100) - torch.tanh(self.d_bar/100)))
 
-    a0, a1 = np.polyfit(pe_d[filtered], gr_d[filtered], deg=1) # linear least squares fit
-    return a0*pe_d + a1
+    '''
+        Sample [num] points from pr_d
+        where [num] is the number of points that we wish to sample.
+    '''
+    def sample_pair_points(self, num):
+        idx = torch.multinomial(self.pr_d, num_samples=num, replacement=True)
+        return self.pr_d[idx]
 
-  '''
-    Get the PWN loss given the ground truths and the normals
-  '''
-  def pairwise_normal_loss(self, n_A, n_B, gt_A, gt_B):
-    pwn = torch.mean(torch.abs(n_A*n_B - gt_A*gt_B))
-    return pwn
-  
-  '''
-    Assume gt_d and d_bar are requires_grad=True.
-    Compute MSG
-  '''
-  def multi_scale_gradient_loss(self, gt_d, d_bar):
-    
-    K = [2, 8, 32, 64]    # k values from the existing paper
-    N = gt_d.size()
+    '''
+        Get the ground truth of a sample [smp]
+        [smp] is a list
+    '''
+    def get_gt_d_of_sample(self, smp):
+        pos = [(self.pr_d == x).nonzero().item() for x in smp]   # get the pos list of the samples from  the pr_d
+        return torch.stack([self.gt_d[x] for x in pos])
 
-    msg = 0
-    gt_d.backward(gradient=torch.tensor([1., 1.]))
-    d_bar.backward(gradient=torch.tensor([1., 1.]))
-    for k in K:
-        msg += torch.abs((gt_d[0].grad)**k - (d_bar[0].grad)**k) +  torch.abs((gt_d[1].grad)**k - (d_bar[1].grad)**k)
+    '''
+        Follows Xian et al's Structure-guided rank loss, which can imporve edge sharpness
+        the sampling method is followed but enforced on surface normal space
+        this improves global and local geometric relations
+    '''
+    def surface_normal(self, sample_pr_d, sample_gt_d):
+        gr_d = sample_gt_d.squeeze()
+        pe_d = sample_pr_d.squeeze()
+        filtered = (gr_d > 1e-8) & (pe_d > 1e-8)
 
-    return msg/N
+        a0, a1 = np.polyfit(pe_d[filtered], gr_d[filtered], deg=1) # linear least squares fit
+        return a0*pe_d + a1
 
-  '''
-    Returns the overall loss.
-  '''
-  def overall_loss(self):
-    lambda_a, lambda_g = 1, 0.5     # given constants in the paper
-    return self.pwn + lambda_a*self.ilnr + lambda_g*self.msg
+    '''
+        Get the PWN loss given the ground truths and the normals
+    '''
+    def pairwise_normal_loss(self, n_A, n_B, gt_A, gt_B):
+        pwn = torch.mean(torch.abs(n_A*n_B - gt_A*gt_B))
+        return pwn
+
+    '''
+        Assume gt_d and d_bar are requires_grad=True.
+        Compute MSG
+    '''
+    def multi_scale_gradient_loss(self):
+
+        K = [2, 8, 32, 64]    # k values from the existing paper
+        N = self.gt_d.size()
+
+        msg = 0
+        self.gt_d.backward(gradient=torch.tensor([1., 1.]))
+        self.d_bar.backward(gradient=torch.tensor([1., 1.]))
+        for k in K:
+            msg += torch.abs((self.gt_d[0].grad)**k - (self.d_bar[0].grad)**k) +  torch.abs((self.gt_d[1].grad)**k - (self.d_bar[1].grad)**k)
+
+        return msg/N
+
+    '''
+        Returns the overall loss.
+    '''
+    def overall_loss_func(self):
+        lambda_a, lambda_g = 1, 0.5     # given constants in the paper
+        return self.pwn + lambda_a*self.ilnr + lambda_g*self.msg
+
+    '''
+        Accessor
+    '''
+    def get_loss(self):
+        return self.overall_loss
