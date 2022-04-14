82e8f96ceba51b5eace610907be455b737707825
Gian Alix <gian.alix@gmail.com>
2022-04-14 18:00:11 -0400

    Cleaned code

--- model/dp_loss.py
+++ model/dp_loss.py
@@ -4,14 +4,12 @@ import torch.nn.init
 import torchvision
 import numpy as np
 
-
-
 class DepthPredictionLoss(nn.Module):
+    
     '''
         pr_d: The predicted depth
         gt_d: The ground truth depth
     '''
-
     def __init__(self, pr_d, gt_d):
         super(DepthPredictionLoss, self).__init__()
 
@@ -27,11 +25,9 @@ class DepthPredictionLoss(nn.Module):
         self.ilnr = self.image_level_normalized_regression_loss()
 
         # Paiwise Normal Loss
-        # sampled_points =  self.sample_pair_points(100000) # 100K sampled points
         sample_pr_d_A, sample_gt_d_A = self.sample_pair_points(
             1000)  # paper uses 100K sampled points but we reduce to 1000 for simplicity and save time
         surface_normal_A = self.surface_normal(sample_pr_d_A, sample_gt_d_A)
-        # sampled_points =  self.sample_pair_points(100000) # 100K sampled points
         sample_pr_d_B, sample_gt_d_B = self.sample_pair_points(
             1000)  # paper uses 100K sampled points but we reduce to 1000 for simplicity and save time
         surface_normal_B = self.surface_normal(sample_pr_d_B, sample_gt_d_B)
@@ -49,7 +45,6 @@ class DepthPredictionLoss(nn.Module):
         This is done to prevent outliers.
         3. Apply the proposed ILNR formula.
     '''
-
     def image_level_normalized_regression_loss(self):
         return torch.mean(
             torch.abs(self.pr_d - self.d_bar) + torch.abs(torch.tanh(self.pr_d / 100) - torch.tanh(self.d_bar / 100)))
@@ -58,25 +53,15 @@ class DepthPredictionLoss(nn.Module):
         Sample [num] points from pr_d and gt_d
         where [num] is the number of points that we wish to sample.
     '''
-
     def sample_pair_points(self, num):
         idx = torch.multinomial(self.pr_d.flatten(), num_samples=num, replacement=True)
         return self.pr_d.flatten()[idx], self.gt_d.flatten()[idx]
-
-    # '''
-    #     Get the ground truth of a sample [smp]
-    #     [smp] is a list
-    # '''
-    # def get_gt_d_of_sample(self, smp):
-    #     pos = [(self.pr_d == x).nonzero().item() for x in smp]   # get the pos list of the samples from  the pr_d
-    #     return torch.stack([self.gt_d[x] for x in pos])
-
+    
     '''
         Follows Xian et al's Structure-guided rank loss, which can imporve edge sharpness
         the sampling method is followed but enforced on surface normal space
         this improves global and local geometric relations
     '''
-
     def surface_normal(self, sample_pr_d, sample_gt_d):
         gr_d = sample_gt_d.squeeze()
         pe_d = sample_pr_d.squeeze()
@@ -88,18 +73,14 @@ class DepthPredictionLoss(nn.Module):
     '''
         Get the PWN loss given the ground truths and the normals
     '''
-
     def pairwise_normal_loss(self, n_A, n_B, gt_A, gt_B):
         pwn = torch.mean(torch.abs(n_A * n_B - gt_A * gt_B))
         return pwn
-
-    # clear
-
+    
     '''
         Assume pr_d and d_bar are requires_grad=True.
         Compute MSG
     '''
-
     def multi_scale_gradient_loss(self):
         N = self.pr_d.flatten().size()[0]
 
@@ -122,7 +103,6 @@ class DepthPredictionLoss(nn.Module):
     '''
         Returns the overall loss.
     '''
-
     def overall_loss_func(self):
         lambda_a, lambda_g = 1, 0.5  # given constants in the paper
         return self.pwn + lambda_a * self.ilnr + lambda_g * self.msg
@@ -130,23 +110,5 @@ class DepthPredictionLoss(nn.Module):
     '''
         Accessor
     '''
-
     def get_loss(self):
         return self.overall_loss
-
-# import cv2
-# import torchvision.transforms as transforms
-
-
-# path = '/content/gdrive/MyDrive/EECS6322/depth_zbuffer/point_0_view_0_domain_depth_zbuffer.png'
-# img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-# d1 = img.shape[0]
-# d2 = img.shape[1]
-# resized_gt_copy = cv2.resize(img, (448, 448)) # resize images to 448 x 448
-# image = cv2.resize(img, (int(d2//2), int(d1//2)))
-# gray_transform = transforms.Compose([transforms.ToPILImage(),
-#                                               transforms.Grayscale(),
-#                                               transforms.ToTensor()])
-# gt_depth = gray_transform(image).cpu().numpy().squeeze()
-
-# print(DepthPredictionLoss(pr_d=gt_depth, gt_d=gt_depth).overall_loss)
